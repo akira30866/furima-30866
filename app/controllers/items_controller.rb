775ac02a70326d2b12c32557bef92908f1a8e80d
akira30866 <a.nishijimayou@gmail.com>
2020-11-02 18:15:11 +0900

    createアクションの設定
    ・itemモデルのスペルミスも修正
    conditon→condition

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,5 +9,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id,:delivery_charge_id, :days_to_ship_id, :prefecture_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,29 +3,29 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  belongs_to :conditon
+  belongs_to :condition
   belongs_to :delivery_charge
   belongs_to :days_to_ship
   belongs_to :prefecture
 
   # ActiveHashのバリデーションに注意
-  VALID_PRICE_REGEX = /\A[0-9]+\z/i
+  VALID_PRICE_REGEX = /\A[0-9]+\z/
 
   with_options presence: true do
     validates :name
     validates :description
     validates :image
     validates :category
-    validates :conditon
+    validates :condition
     validates :delivery_charge
     validates :prefecture
     validates :days_to_ship
-    validates :price, format: { with: VALID_PRICE_REGEX, message: "半角数字で入力してください" }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :price, format: { with: VALID_PRICE_REGEX }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
 
-  with_options numericality: { other_than: 1 } do
+  with_options numericality: { other_than: 1, message: "選択してください" } do
     validates :category_id
-    validates :conditon_id
+    validates :condition_id
     validates :delivery_charge_id
     validates :days_to_ship_id
     validates :prefecture_id
