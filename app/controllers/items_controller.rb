8e2f74fc875c7061a3217c0fe151e08c75e8432f
akira30866 <a.nishijimayou@gmail.com>
2020-11-02 11:19:29 +0900

    商品投稿ページへのルーティングの設定
    ・aタグをヘルパーメソッドに変更
    ・itemのnew.html.erbを適切なフォルダに配置
    ・ログインユーザーのみが商品出品ページへ遷移でき、未ログインユーザーはログインページへ遷移するよう設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  # アクションの設定については実装に応じて追記
+  before_action :authenticate_user!, except: [:index]
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index" 
+  # 機能実装ごとにアクションは設定する
+  resources :items, only: [:index, :new]
 end
