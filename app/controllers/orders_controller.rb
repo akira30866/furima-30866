167002d75ac92b851b1c535f6d2ca1e1e1adb907
akira30866 <a.nishijimayou@gmail.com>
2020-11-05 20:33:49 +0900

    ordersコントローラの処理を設定
    ・index,createアクションを設定
    ・privateにorder_address_params作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,9 +2,23 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
   
+  private
+  
+  def order_address_params
+    params.require(:order_address).permit(:postal_number, :prefecture_id, :city, :house_number, :building_number, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+  end
+
 end
