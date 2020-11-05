5ea42db0dbdccaef59429fcfa6af309151f47d22
akira30866 <a.nishijimayou@gmail.com>
2020-11-05 20:14:40 +0900

    order_addressモデルの修正
    ・validates: prefecture_idの後に","を追加
    ・attr_accessorに:user_id,:item_idを追加
    ・current_user.idをuser_idへ変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_number, :prefecture_id, :city, :house_number, :building_number, :phone_number
+  attr_accessor :postal_number, :prefecture_id, :city, :house_number, :building_number, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
@@ -9,10 +9,10 @@ class OrderAddress
     validates :house_number
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/ }
   end
-  validates :prefecture_id numericality: { other_than: 1, message: "選択してください" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "選択してください" }
 
   def save
-    order = Order.create(item_id: item_id, user_id: current_user.id)
+    order = Order.create(item_id: item_id, user_id: user_id)
     Address.create(postal_number: postal_number, prefecture_id: prefecture_id, city: city, house_number: house_number, building_number: building_number, phone_number: phone_number, order_id: order.id)
   end
 end
