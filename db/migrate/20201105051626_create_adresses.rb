d34b041e8d95a5c92cf89b1134e6bbf702f4e7d7
akira30866 <a.nishijimayou@gmail.com>
2020-11-05 14:25:12 +0900

    ordersテーブル、addressesテーブルの作成

---
+++ db/migrate/20201105051626_create_adresses.rb
@@ -0,0 +1,15 @@
+class CreateAdresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :adresses do |t|
+
+      t.timestamps
+      t.string  :postal_number, null: false
+      t.integer :prefecture_id, null: false
+      t.string  :city,          null: false
+      t.string  :house_number,  null: false
+      t.string  :building_number
+      t.string  :phone_number,  null: false
+      t.references :order,      foreign_key: true
+    end
+  end
+end
