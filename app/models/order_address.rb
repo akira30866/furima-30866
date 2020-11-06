bbb9365a2ab8e01b28be7e1107595621cbb8eb07
akira30866 <a.nishijimayou@gmail.com>
2020-11-06 12:34:11 +0900

    バリデーションの修正
    prefecture_idで管理しているので削除

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,7 +4,6 @@ class OrderAddress
 
   with_options presence: true do
     validates :postal_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :prefecture
     validates :city
     validates :house_number
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/ }
