f2490eb3eccf88f7f148cfaf4b22b5c005bd4ae9
akira30866 <a.nishijimayou@gmail.com>
2020-11-06 16:40:42 +0900

    ファクトリーボットの設定

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_number   { '123-4567' }
+    prefecture_id   { 2 }
+    city            { '横浜' }
+    house_number    { '青山' }
+    building_number { '柳ビル' }
+    phone_number    { '00012345678' }
+  end
+end
