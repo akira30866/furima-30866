b5005eec4f3ae10fd03f6460e1812d9f8ce6cda1
akira30866 <a.nishijimayou@gmail.com>
2020-11-09 12:32:37 +0900

    クレジットカードに関する機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,14 @@
 class OrdersController < ApplicationController
+  before_action :find_item
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
     @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -18,7 +19,20 @@ class OrdersController < ApplicationController
   private
   
   def order_address_params
-    params.require(:order_address).permit(:postal_number, :prefecture_id, :city, :house_number, :building_number, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:order_address).permit(:postal_number, :prefecture_id, :city, :house_number, :building_number, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
+  end
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ""
+    Payjp::Charge.create(
+      amount:   @item.price,
+      card:     order_address_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,12 +1,13 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_number, :prefecture_id, :city, :house_number, :building_number, :phone_number, :user_id, :item_id
+  attr_accessor :postal_number, :prefecture_id, :city, :house_number, :building_number, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :city
     validates :house_number
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/ }
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "選択してください" }
 
